26d4a0c7cfb032901870e60410906b8e37f0d7ef
Anurag Sen <anurag007sen@gmail.com>
2023-09-06 09:00:18 +0530

    All set

---
+++ app/src/main/java/as/example/dobcalculator/dicerolled/MainActivity.kt
@@ -0,0 +1,44 @@
+package `as`.example.dobcalculator.dicerolled
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    var btnRoll : Button ? = null
+    lateinit var diceImage : ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnRoll = findViewById(R.id.roll_button);
+
+        btnRoll?.setOnClickListener {
+            diceRolled ()
+        }
+    }
+
+    fun diceRolled() {
+
+        diceImage = findViewById(R.id.dice_image)
+        var randomInt = java.util.Random().nextInt(6) + 1
+
+        var drowableResource  = when (randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drowableResource)
+
+
+
+    }
+}
